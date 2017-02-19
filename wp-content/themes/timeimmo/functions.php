26c6ab44cd9e2a4beace150086d511f3096ab6dc
jimmy <jimmy.segers@idweaver.com>
2017-02-19 22:07:41 +0100

    install theme

---
+++ wp-content/themes/timeimmo/functions.php
@@ -0,0 +1,10 @@
+<?php add_theme_support( 'post-thumbnails'); ?>
+ 
+<!-- enleve la taille hauteur des thumbnail -->
+<?php add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+ 
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+} ?>
---
+++ wp-content/themes/timeimmo/header.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<head>
+
+<!-- Basic Page Needs
+================================================== -->
+<title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<!-- CSS
+================================================== -->
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css">
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/colors/main.css" id="colors">
+
+</head>
+
+<body>
+
+<!-- Wrapper -->
+<div id="wrapper">
+
+
+<!-- Header Container
+================================================== -->
+<header id="header-container" class="header-style-2">
+
+	<!-- Header -->
+	<div id="header">
+		<div class="container">
+			
+			<!-- Left Side Content -->
+			<div class="left-side">
+				
+				<!-- Logo -->
+				<div id="logo" class="margin-top-10">
+					<a href="index.html"><img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt=""></a>
+
+					<!-- Logo for Sticky Header -->
+					<a href="index.html" class="sticky-logo"><img src="images/logo2.png" alt=""></a>
+				</div>
+				
+			</div>
+			<!-- Left Side Content / End -->
+
+			<!-- Right Side Content / End -->
+			<div class="right-side">
+				<ul class="header-widget">
+					<li>
+						<i class="sl sl-icon-call-in"></i>
+						<div class="widget-content">
+							<span class="title">Questions?</span>
+							<span class="data">(123) 123-456 </span>
+						</div>
+					</li>
+
+					<li>
+						<i class="sl sl-icon-location"></i>
+						<div class="widget-content">
+							<span class="title">Our Office</span>
+							<span class="data">45 Park Avenue, NY</span>
+						</div>
+					</li>
+
+					<li class="with-btn"><a href="submit-property.html" class="button border">Submit Property</a></li>
+				</ul>
+			</div>
+			<!-- Right Side Content / End -->
+
+		</div>
+
+
+		<!-- Mobile Navigation -->
+		<div class="menu-responsive">
+			<i class="fa fa-reorder menu-trigger"></i>
+		</div>
+
+
+		<!-- Main Navigation -->
+		<nav id="navigation" class="style-2">
+			<div class="container">
+					<ul id="responsive">
+
+						<li><a class="current" href="#">Home</a>
+							<ul>
+								<li><a href="index.html">Home 1</a></li>
+								<li><a href="index-2.html">Home 2</a></li>
+								<li><a href="index-3.html">Home 3</a></li>
+								<li><a href="index-4.html">Home 4</a></li>
+							</ul>
+						</li>
+
+						<li><a href="#">Listings</a>
+							<ul>
+								<li><a href="#">List Layout</a>
+									<ul>
+										<li><a href="listings-list-with-sidebar.html">With Sidebar</a></li>
+										<li><a href="listings-list-with-map.html">With Map</a></li>
+										<li><a href="listings-list-full-width.html">Full Width</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Grid Layout</a>
+									<ul>
+										<li><a href="listings-grid-standard-with-sidebar.html">Standard With Sidebar</a></li>
+										<li><a href="listings-grid-compact-with-sidebar.html">Compact With Sidebar</a></li>
+										<li><a href="listings-grid-with-map.html">With Map</a></li>
+										<li><a href="listings-grid-full-width.html">Full Width</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Half Map</a>
+									<ul>
+										<li><a href="listings-half-map-list.html">List Layout</a></li>
+										<li><a href="listings-half-map-grid-standard.html">Grid Standard Layout</a></li>
+										<li><a href="listings-half-map-grid-compact.html">Grid Compact Layout</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+
+						<li><a href="#">Features</a>
+							<ul>
+								<li><a href="#">Single Properties</a>
+									<ul>
+										<li><a href="single-property-page-1.html">Property Style 1</a></li>
+										<li><a href="single-property-page-2.html">Property Style 2</a></li>
+										<li><a href="single-property-page-3.html">Property Style 3</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Search Styles</a>
+									<ul>
+										<li><a href="index.html">Home Search 1</a></li>
+										<li><a href="index-2.html">Home Search 2</a></li>
+										<li><a href="index-3.html">Home Search 3</a></li>
+										<li><a href="listings-list-full-width.html">Advanced Style</a></li>
+										<li><a href="listings-list-with-sidebar.html">Sidebar Search</a></li>
+									</ul>
+								</li>
+								<li><a href="#">My Account</a>
+									<ul>
+										<li><a href="my-profile.html">My Profile</a></li>
+										<li><a href="my-bookmarks.html">Bookmarked Listings</a></li>
+										<li><a href="my-properties.html">My Properties</a></li>
+										<li><a href="change-password.html">Change Password</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Agents</a>
+									<ul>
+										<li><a href="agents-list.html">Agents List</a></li>
+										<li><a href="agent-page.html">Agent Page</a></li>
+									</ul>
+								</li>
+
+								<li><a href="submit-property.html">Submit Property</a></li>
+							</ul>
+						</li>
+
+						<li><a href="#">Pages</a>
+							<ul>
+								<li><a href="blog.html">Blog</a>
+									<ul>
+										<li><a href="blog.html">Blog</a></li>
+										<li><a href="blog-post.html">Blog Post</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact</a></li>
+								<li><a href="elements.html">Elements</a></li>
+								<li><a href="pricing-tables.html">Pricing Tables</a></li>
+								<li><a href="typography.html">Typography</a></li>
+								<li><a href="icons.html">Icons</a></li>
+							</ul>
+						</li>
+
+					</ul>
+			</div>
+		</nav>
+		<div class="clearfix"></div>
+		<!-- Main Navigation / End -->
+	</div>
+	<!-- Header / End -->
+
+</header>
+<div class="clearfix"></div>
+<!-- Header Container / End -->
